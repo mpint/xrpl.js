0888de50949b6a2e4be3bef2600d3a1041ebfea3
Michael Pinter <mpinter@ripple.com>
2026-03-02 15:47:56 -0700

    Refactor NFTBuyOffers to use pagination base types

--- packages/xrpl/src/models/methods/nftBuyOffers.ts
+++ packages/xrpl/src/models/methods/nftBuyOffers.ts
@@ -1,6 +1,12 @@
 import { NFTOffer } from '../common'
 
-import { BaseRequest, BaseResponse, LookupByLedgerRequest } from './baseMethod'
+import {
+  BaseRequest,
+  BaseResponse,
+  LookupByLedgerRequest,
+  PaginationRequest,
+  PaginationResponse,
+} from './baseMethod'
 
 /**
  * The `nft_buy_offers` method retrieves all of buy offers for the specified
@@ -9,7 +15,7 @@ import { BaseRequest, BaseResponse, LookupByLedgerRequest } from './baseMethod'
  * @category Requests
  */
 export interface NFTBuyOffersRequest
-  extends BaseRequest, LookupByLedgerRequest {
+  extends BaseRequest, LookupByLedgerRequest, PaginationRequest {
   command: 'nft_buy_offers'
   /**
    * The unique identifier of an NFToken. The request returns buy offers for this NFToken.
@@ -32,5 +38,5 @@ export interface NFTBuyOffersResponse extends BaseResponse {
      * The token ID of the NFToken to which these offers pertain.
      */
     nft_id: string
-  }
+  } & PaginationResponse
 }
